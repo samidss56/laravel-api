a79019c65a3c3be771edcfc225f66393cf947238
samidss56 <adimas.id@gmail.com>
2024-05-06 13:20:52 +0700

    todos API using laravel

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todos = Todo::get();
+
+        return response()->json($todos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $todo = Todo::create([
+            'title' => request('title'),
+            'status' => 0,
+        ]);
+
+        return response()->json($todo);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $todo = Todo::find($id);
+
+        return response()->json($todo);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $todo = Todo::find($id);
+
+        $todo->update([
+            'title' => request('title', $todo->title),
+            'status' => request('status', $todo->status),
+        ]);
+
+        return response()->json($todo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $todo = Todo::find($id);
+
+        $todo->delete();
+
+        return response()->json([
+            'message' => 'Todo deleted successfully',
+        ]);
+    }
+}
